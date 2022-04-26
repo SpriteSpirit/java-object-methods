ac304f543fc26909b3a71507ede14446b77b0fa4
Sprite_Spirit <a.backer@yandex.com>
2022-04-26 12:30:43 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        PersonService personService = new PersonService();
+
+        Person sarah = new Person("Sarah", 30);
+        sarah.setName("Monika");
+        sarah.increaseAge(2);
+        if (personService.isAdult(sarah)) { // метод класса
+            System.out.println("Go to bar!");
+        } else {
+            System.out.println("Go to school!");
+        }
+        System.out.println(sarah);
+
+
 
 
     }
